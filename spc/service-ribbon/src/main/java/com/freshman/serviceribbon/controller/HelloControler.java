6343fc756d1fb44830646231da31444720deb70e
FreshMan <qinbocai@sina.cn>
2019-01-23 17:42:25 +0800

    https://www.fangzhipeng.com/springcloud/2018/08/30/sc-f2-ribbon/

---
+++ spc/service-ribbon/src/main/java/com/freshman/serviceribbon/controller/HelloControler.java
@@ -0,0 +1,18 @@
+package com.freshman.serviceribbon.controller;
+
+import com.freshman.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloControler {
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+}
